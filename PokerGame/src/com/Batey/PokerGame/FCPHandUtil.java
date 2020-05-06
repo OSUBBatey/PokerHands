c8b6e445290e5c1f586e14792cfd249b37799ebe
Brian Batey <batey.18@osu.edu>
2020-05-06 12:30:38 -0400

    Update FCPHandUtil.java
    Moved flush test code to individual function

--- PokerGame/src/com/Batey/PokerGame/FCPHandUtil.java
+++ PokerGame/src/com/Batey/PokerGame/FCPHandUtil.java
@@ -260,6 +260,7 @@ public class FCPHandUtil {
 			//Tracking Flags
 			boolean isStraight = true; //If non-consecutive rank is found set to false
 			boolean isFlush = true; //If any suit is different set to false
+			Character heldSuit = null; //Suit value for flush tracking	
 			
 			Integer lastInt = null; //Track the value of the last card for straight tracking
 			
@@ -267,7 +268,7 @@ public class FCPHandUtil {
 			Character lowGroupRank = null, highGroupRank = null; //Track (high/low) group ranks
 			Card lowGroupCard = null, highGroupCard = null; //Store (high/low) group Card object
 			
-			Character heldSuit = null; //Suit value for flush tracking			
+					
 			
 			FCPResult output = null; //Function output			
 			
@@ -282,14 +283,12 @@ public class FCPHandUtil {
 							isStraight = false;
 					}
 				}
-				
-				if(isFlush) {
-					if(heldSuit == null) {
-						heldSuit = card.getSuit().getSuitChar();
-					}else if(heldSuit != card.getSuit().getSuitChar()) {
-							isFlush = false;
-					}
-				}			
+							
+				if(heldSuit == null) { //Populate heldSuit if null
+					heldSuit = card.getSuit().getSuitChar();
+				}
+				isFlush = flushTest(isFlush, card, heldSuit); //Test for flush
+						
 				
 				if(!rankCounts.containsKey(currentRank)) { //If current card rank doesn't exist in the map, add it.
 					rankCounts.put(currentRank, 1); //Set count to one
@@ -302,12 +301,16 @@ public class FCPHandUtil {
 						lowGroupCount = highGroupCount; //Set small group count
 						highGroupCard = card; //Set large group high card
 						lowGroupCard = card; // Set small group high card
-					}else if(rankCounts.get(currentRank)> rankCounts.get(highGroupRank)) {//If current grouping is larger than stored large group, update with current.						
-						lowGroupCard = highGroupCard;
-						lowGroupCount = highGroupCount;
-						highGroupRank = currentRank;
-						highGroupCard = card;						
-						highGroupCount = rankCounts.get(highGroupRank);
+					}else if(rankCounts.get(currentRank)> highGroupCount) {//If current grouping is larger than stored large group, update with current.						
+						
+						if(currentRank != highGroupRank) { //If ranks are different update instance variables
+							lowGroupCard = highGroupCard;
+							lowGroupCount = highGroupCount;
+							highGroupRank = currentRank;
+							highGroupCard = card;
+						}
+						highGroupCount = rankCounts.get(highGroupRank); //Update highGroup count 
+						
 					}else if(currentRank != highGroupRank &&rankCounts.get(currentRank) == rankCounts.get(lowGroupRank)) {//If current grouping size is equal to stored large grouping and ranks are different
 						
 						if(card.rankToInt(currentRank) > card.rankToInt(highGroupRank)) {//If Current card rank is higher than large grouping stored rank
@@ -325,32 +328,53 @@ public class FCPHandUtil {
 				}				
 			}
 			
-			if(isFlush && isStraight) {
+			//Determine FCP Score Rank based off flags
+			if(isFlush && isStraight) {//Straight Flush
 				output = new FCPResult( FCPScoreRank.S_FLUSH, player.getHighCard());
-			}else if(isStraight) {
+			}else if(isStraight) {//Straight
 				output = new FCPResult(FCPScoreRank.STRAIGHT, player.getHighCard());
-			}else if(isFlush) {
+			}else if(isFlush) {//Flush
 				output = new FCPResult(FCPScoreRank.FLUSH, player.getHighCard());
-			}else if(highGroupCount == 4) {
+			}else if(highGroupCount == 4) {// Four-of-a-kind
 				output = new FCPResult(FCPScoreRank.FOUR_OF_A_KIND, player.getHighCard());
-			}else if(highGroupCount == 3) {
-				if(lowGroupCount == 2) {
+			}else if(highGroupCount == 3) {//A grouping of three exists
+				if(lowGroupCount == 2) { //If a grouping of two also exists, then full house
 					output = new FCPResult(FCPScoreRank.FULL_HOUSE, highGroupCard, lowGroupCard);
-				}else {
+				}else {//Else, Three-of-a-kind
 					output = new FCPResult(FCPScoreRank.THREE_OF_A_KIND, highGroupCard);
 				}
-			}else if(highGroupCount == 2) {
-				if(highGroupCount == lowGroupCount) {
+			}else if(highGroupCount == 2) {//Pair exists
+				if(highGroupCount == lowGroupCount) { //If a pair exists and another grouping has the same count then two pairs must exist.
 					output = new FCPResult(FCPScoreRank.TWO_PAIR, highGroupCard, lowGroupCard);
-				}else {
-					//TODO: ERROR CHECKING FOR THIS , I DONT THINK A BRANCH EXISTS FOR SOMETHING
+				}else {	//Else single Pair			
 					output = new FCPResult(FCPScoreRank.PAIR, highGroupCard, lowGroupCard);
 				}
-			}else {
+			}else {//Otherwise must be a high card result
 				output = new FCPResult(FCPScoreRank.HIGH_CARD, player.getHighCard());
 			}
 			return output;
 		}
+	}
+	
+	/**
+	 * Tests if the given Card suit is equal to the given heldSuit.
+	 * @param isFlush
+	 * 		- a boolean representing the current flush state, true if still possible, false otherwise
+	 * @param card
+	 * 		- a Card object from which to get a suit for testing
+	 * @param heldSuit
+	 * 		- a char object representing the the currently held suit value
+	 * @return
+	 * 		- a boolean value representing the current flush state, true if still possible, false otherwise
+	 */
+	private Boolean flushTest(Boolean isFlush, Card card, Character heldSuit) {
+		Boolean output = isFlush;
+		if(isFlush) {
+			if(heldSuit != card.getSuit().getSuitChar()) {
+				output = false;
+			}
+		}
+		return output;
 	}
 	/**
 	 * 
